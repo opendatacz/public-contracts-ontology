820be0aa8b28b77869e95f5bfc43f408a865cd1a
Jind?ich Mynarz <mynarzjindrich@gmail.com>
2013-04-02 10:14:06 +0200

    Removed transformer

--- transformers/GB-notices/transform_sample.bat
+++
@@ -1,2 +0,0 @@
-REM java -jar "c:\Program Files\Saxon\saxon9he.jar" -s:notices.xml -xsl:notices2pc.xslt -o:pc.rdf
-java -jar "c:\Program Files\Saxon\saxon9he.jar" -s:sample_01.xml -xsl:notices2pc.xslt -o:sample_01.rdf
